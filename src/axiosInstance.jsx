959a795c1a33c6d2a7fa2cd8497b02a9dda9b5c2
Dilshad <dilshadalicp28@gmail.com>
2025-04-15 21:49:40 +0530

    changed

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -1,13 +1,11 @@
 
-// src/axiosInstance.js
 import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000",  // change as per your backend URL
+  baseURL: "http://127.0.0.1:8000",  
 });
 
-// Add token to every request
 axiosInstance.interceptors.request.use((config) => {
   const token = Cookies.get("accesstoken");
   if (token) {
